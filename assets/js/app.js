e366fec61a44da58998d6ac0860fc7e103e4ff07
tauqir-rabbani <tauqir.pbhub003@gmail.com>
2024-04-24 20:01:17 +0500

    One Portfolio page is completed

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,33 +1,32 @@
 function themeChange() {
   let btn = document.getElementById("checkbox");
 
-  if(btn.checked){
+  if (btn.checked) {
     changeColor();
-  }else{
-    changeColor();    
+  } else {
+    changeColor();
   }
 }
 
 function changeColor() {
   let pElemet = document.getElementsByTagName("p");
   var root = document.documentElement;
-  var primaryColor = getComputedStyle(root).getPropertyValue('--basic-purple');
+  var primaryColor = getComputedStyle(root).getPropertyValue("--basic-purple");
 
-  if (primaryColor === '#181757') {
+  if (primaryColor === "#181757") {
     for (const elements of pElemet) {
-      if(elements.classList.contains("text-white")){
-        elements.classList.toggle("text-white","text-dark")
+      if (elements.classList.contains("text-white")) {
+        elements.classList.toggle("text-white", "text-dark");
       }
     }
-    root.style.setProperty('--basic-purple', '#fff');
-    
+    root.style.setProperty("--basic-purple", "#fff");
   } else {
     for (const elements of pElemet) {
-      if(elements.classList.contains("text-dark")){
-        elements.classList.toggle("text-dark","text-white")
+      if (elements.classList.contains("text-dark")) {
+        elements.classList.toggle("text-dark", "text-white");
       }
     }
-      root.style.setProperty('--basic-purple', '#181757');
+    root.style.setProperty("--basic-purple", "#181757");
   }
 }
 
@@ -35,16 +34,16 @@ let counter = document.getElementById("counter");
 let timers = document.getElementsByClassName("timer");
 let timeVal = [750, 23, 200, 28];
 let countdownCalled = false;
-
-window.addEventListener("scroll", () => {
-  if (!countdownCalled && eval(window.scrollY+450) >= counter.offsetTop) {
-    countdownCalled = true;
-    for (let i = 0; i < timers.length; i++) {
-      countDown(timers[i], timeVal[i]);
+if (counter) {
+  window.addEventListener("scroll", () => {
+    if (!countdownCalled && eval(window.scrollY + 450) >= counter.offsetTop) {
+      countdownCalled = true;
+      for (let i = 0; i < timers.length; i++) {
+        countDown(timers[i], timeVal[i]);
+      }
     }
-  }
-});
-
+  });
+}
 function countDown(elements, values) {
   let j = 0;
   let interval = setInterval(() => {
