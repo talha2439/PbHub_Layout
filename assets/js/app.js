dbd85d077d837338e56ffa7a0a00796b1302cefd
tauqir-rabbani <tauqir.pbhub003@gmail.com>
2024-04-29 15:11:24 +0500

    Completed

--- assets/js/app.js
+++ assets/js/app.js
@@ -55,3 +55,23 @@ function countDown(elements, values) {
     }
   }, 20);
 }
+
+if (document.getElementById("carouselExample")) {
+  document.addEventListener("DOMContentLoaded", function () {
+    var carouselItems = document.querySelectorAll(".carousel .carousel-item");
+    carouselItems.forEach(function (item) {
+      var minPerSlide = 3;
+      var next = item.nextElementSibling;
+
+      item.appendChild(next.firstElementChild.cloneNode(true));
+
+      for (var i = 0; i < minPerSlide; i++) {
+        next = next.nextElementSibling;
+        if (!next) {
+          next = item.parentElement.firstElementChild;
+        }
+        item.appendChild(next.firstElementChild.cloneNode(true));
+      }
+    });
+  });
+}
